71aeffa3c21e9e0afa6c8969399651cf85dcf4c0
Amaan Sheikh <100550299+amaan784@users.noreply.github.com>
2024-02-25 23:53:34 -0500

    initial commit with first 5 problems

---
+++ HackerRank/1_if_else.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    nmod = n % 2 
+    if (nmod != 0) or (nmod == 0 and n >= 6 and n <= 20):
+        print("Weird")
+    elif (nmod == 0 and n >= 2 and n <= 5) or (nmod == 0 and n > 20):
+        print("Not Weird")
