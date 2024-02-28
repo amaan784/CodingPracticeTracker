42fc1f217ef59ba383d6db8d5f0363e2742425fb
Amaan Sheikh <100550299+amaan784@users.noreply.github.com>
2024-02-27 23:52:04 -0500

    list comprehension and runner up

--- HackerRank/7_list_comprehensions.py
+++ HackerRank/7_list_comprehensions.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
+    # creates the permutations of 0 to x, 0 to y, 0 to z where the summation of i,j,k is not equal to n
     print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n])
